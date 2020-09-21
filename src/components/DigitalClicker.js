51fa852355c1e79cc212a9bcf27e02a22057cc35
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-21 08:15:28 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -6,6 +6,9 @@ class DigitalClicker extends React.Component {
       timesClicked: 0,
     };
   }
+  handleClick = () => {
+      this.setState({timesClicked: timesClicked+1})
+  render() { return <button onClick={this.handleClick}>{this.state.timesClicked}</button>;}
 }
 
 export default DigitalClicker;
