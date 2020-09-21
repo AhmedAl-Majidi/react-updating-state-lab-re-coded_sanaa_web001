5b020cc448e9565368c94d682183c7c31ae60ea8
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-21 08:13:42 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,11 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    state = {
+      timesClicked: 0,
+    };
+  }
+}
+
+export default DigitalClicker;
